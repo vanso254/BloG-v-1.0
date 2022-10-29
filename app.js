069713080643fe8b0baf3ffcc0e64b8e23cd9fbd
Evans Oduor <oduorevans9@gmail.com>
2022-10-29 09:44:41 +0300

    Creating a blog  home route

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
-    res.send('Home route')
+    const articles=[{
+        title:"Sakaja Wins...",
+        createdAt:new Date(),
+        link:"link",
+        Descripttion:"Sakaja wins elections",
+        markdown:"Sakaja wins elections"
+    },
+    {
+        title:"Sakaja Wins... 2",
+        createdAt:new Date(),
+        link:"link",
+        Descripttion:"Sakaja wins elections2",
+        markdown:"Sakaja wins elections2"
+    }
+]
+    res.render('articles/index',{articles:articles})
 })
 
 app.listen(4040)
